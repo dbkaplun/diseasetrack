8f4bf0124622e4c3f256e7abea6da2d96da28ae7
Dan Kaplun <dan@secrettriangle.net>
2015-01-16 00:04:47 -0600

    Frontend

---
+++ assets/js/layout.js
@@ -0,0 +1,26 @@
+(function (root) {
+  require.config({
+    paths: {
+      jquery: 'https://code.jquery.com/jquery-1.11.1.min',
+      bootstrap: '//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min',
+      // 'socket.io': 'https://cdn.socket.io/socket.io-1.2.1',
+      // 'sails.io': 'https://rawgit.com/balderdashy/sails.io.js/master/dist/sails.io',
+      'sails.io': '/js/dependencies/sails.io',
+      vue: 'https://cdnjs.cloudflare.com/ajax/libs/vue/0.11.4/vue.min',
+      moment: 'https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.9.0/moment.min',
+      binarysearch: 'https://rawgit.com/secrettriangle/javascript-algorithms/240b393f7681b096a81a7680e9e27a7f621d9329/src/searching/binarysearch',
+    },
+    shim: {
+      bootstrap: {deps: ['jquery']},
+      binarysearch: {exports: 'binarySearch'},
+    }
+  });
+
+  root.document.dispatchEvent(new Event('diseasetrackReady'));
+
+  require(['jquery', 'bootstrap'], function (jQuery) {
+    jQuery(function ($) {
+      var $body = $('body').removeClass('no-js').addClass('js');
+    });
+  });
+}(this));
