95fd84daa33120b82e1f290a94480ab20af987dc
Dan Kaplun <dan@secrettriangle.net>
2015-01-17 03:31:02 -0600

    Map is now fullscreen and clustered; implements popups

--- assets/js/layout.js
+++ assets/js/layout.js
@@ -7,14 +7,18 @@
       // 'sails.io': 'https://rawgit.com/balderdashy/sails.io.js/master/dist/sails.io',
       'sails.io': '/js/dependencies/sails.io',
       vue: 'https://cdnjs.cloudflare.com/ajax/libs/vue/0.11.4/vue.min',
-      moment: 'https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.9.0/moment.min',
       leaflet: 'http://cdn.leafletjs.com/leaflet-0.7.3/leaflet',
-      binarysearch: 'https://rawgit.com/mgechev/javascript-algorithms/master/src/searching/binarysearch',
+      'leaflet.markercluster': 'https://rawgit.com/Leaflet/Leaflet.markercluster/master/dist/leaflet.markercluster',
+      'leaflet.geosearch': 'https://rawgit.com/smeijer/L.GeoSearch/master/src/js/l.control.geosearch',
+      'leaflet.geosearch.provider.openstreetmap': 'https://rawgit.com/smeijer/L.GeoSearch/master/src/js/l.geosearch.provider.openstreetmap',
+      moment: 'https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.9.0/moment.min',
       'twitter-text': 'https://rawgit.com/twitter/twitter-text/master/js/twitter-text',
     },
     shim: {
       bootstrap: {deps: ['jquery']},
-      binarysearch: {exports: 'binarySearch'},
+      'leaflet.markercluster': {deps: ['leaflet']},
+      'leaflet.geosearch': {deps: ['leaflet']},
+      'leaflet.geosearch.provider.openstreetmap': {deps: ['leaflet.geosearch']},
     }
   });
 
